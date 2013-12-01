6e5874096e158daafa5e763e97cc274244a8f985
Fran Hrženjak <fran@changeset.hr>
2013-12-01 02:45:44 +0100

    “priority” field is optional

--- core/models.py
+++ core/models.py
@@ -4,7 +4,7 @@ from django.db import models
 
 class Item(models.Model):
     user = models.ForeignKey(get_user_model(), blank=True, null=True)  # handled in: api.serializers.ItemSerializer.save_object()
-    priority = models.PositiveIntegerField(name=u'priority', default=0, blank=True, null=False)
+    priority = models.PositiveIntegerField(name=u'priority', default=None, blank=True, null=True)
     due_date = models.DateField(name=u'due_date', default=None, blank=True, null=True)
     completed = models.BooleanField(name=u'completed', default=False, blank=True, null=False)
     memo = models.TextField(name=u'memo', default=u'', blank=False, null=False)
